09a494321722ab13495335e6e22b66c0cf88d7d1
Ahmad Adillaumam <44931897+wewnumam@users.noreply.github.com>
2023-11-13 00:32:17 +0700

    Update pong_game.sql

--- pong_game.sql
+++ pong_game.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `pong_game`
 --
+CREATE DATABASE IF NOT EXISTS `pong_game` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `pong_game`;
 
 -- --------------------------------------------------------
 
